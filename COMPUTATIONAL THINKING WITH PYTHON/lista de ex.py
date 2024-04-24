badd5e204355a9c647c4cc16022b0662c3f44b6a
Eric Ito <84513383+EricYj15@users.noreply.github.com>
2024-04-24 18:07:35 -0300

    Add files via upload

---
+++ COMPUTATIONAL THINKING WITH PYTHON/lista de ex.py
@@ -0,0 +1,58 @@
+# # Solicita ao usuário um número
+# numero = int(input("Digite um número: "))
+
+# # Verifica se o número é par ou ímpar
+# if numero % 2 == 0:
+#     print("O número é par.")
+# else:
+#     print("O número é ímpar.")
+
+
+
+#Escreva um programa que leia dois números e que imprima o maior e o menor.
+# x = int(input("Digite o primeiro número: "))
+# y = int(input("Digite o segundo número: "))
+
+# if x > y:
+#     print(x ,y)
+# else:
+#     print(y, x)
+
+
+#Faça um programa para imprimir de 1 até o número digitado pelo usuário, mostrando apenas os números pares
+# numero = int(input("Digite um número: "))
+# i = 1
+
+# while i <= numero:
+#     if i % 2 == 0:
+#         print(i)
+#     i = i + 1
+
+
+
+#Faça um programa para imprimir de 1 até o número digitado pelo usuário, mostrando apenas os números ímpares.
+# numero = int(input('Digite o numero: '))
+# i = 1
+
+# while i <= numero:
+#     if i % 2 != 0:
+#         print(i)
+
+#     i = i + 1
+
+
+
+#Faça um programa para somar todos os pares dentro de um intervalo numérico passado pelo usuário.
+# inicio = int(input('insira o numero: '))
+# fim = int(input('insira o numero: '))
+
+# soma = 0
+
+
+# i = inicio
+# while i <= fim:
+#     if i % 2 == 0:
+#         soma = soma + i
+#     i = i + 1
+
+# print(soma)
